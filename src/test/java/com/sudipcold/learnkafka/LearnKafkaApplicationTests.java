184322c81c89ddf63847a312c558cae8b892deb1
sudipcold2 <sudipcold2@gmail.com>
2024-04-06 18:41:55 +0530

    init repo

---
+++ src/test/java/com/sudipcold/learnkafka/LearnKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudipcold.learnkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
